dc418738c3e93e48a305e006836b45ee6083f861
LIU CHIAWEI <doublecheap@gmail.com>
2025-11-12 10:17:56 +0900

    clean default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-gray-100">
-      BBS
+      <h1 className="text-5xl font-bold text-center">BBS</h1>
     </main>
   );
 }
