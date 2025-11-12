333b25abf9644cda01686c810a292d2994481ec1
LIU CHIAWEI <doublecheap@gmail.com>
2025-11-12 10:28:51 +0900

    feat/storybook: init storybook for next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen w-full bg-gray-100">
+    <main className="min-h-screen w-full flex items-center justify-center">
       <h1 className="text-5xl font-bold text-center">BBS</h1>
     </main>
   );
